cd42ed3522b97e882b4ed995d9aa4435d3019620
Coriano Harris <corianoharrispro@gmail.com>
2025-01-28 22:24:30 -0600

    added robot fade in experience response

--- src/app/components/shared/Messages.tsx
+++ src/app/components/shared/Messages.tsx
@@ -10,48 +10,52 @@ interface MessagesProps
 
 export function Messages({ messages }: MessagesProps)
 {
+    
+    
     const sortedMessages = messages.sort((a, b) => b.timestamp - a.timestamp);
 
     return (
         <div
-            className="mb-4 h-64 overflow-y-auto space-y-2 border-b pb-2"
+            className="mb-4 mt-4 h-64 overflow-y-auto space-y-4 border-b pb-2"
             data-testid="message-list"
         >
             {sortedMessages.map((msg) => (
                 <div
                     key={msg.id}
-                    className={`p-3 rounded-lg shadow-md ${msg.sentiment === 'positive'
+                    className={` p-3 rounded-lg shadow-md ${msg.sentiment === 'positive'
                         ? 'bg-green-50 border-green-200'
                         : msg.sentiment === 'negative'
                             ? 'bg-red-50 border-red-200'
                             : 'bg-gray-50 border-gray-200'
                         } border`}
                 >
-                    {msg.score >= 2 && (
-                        <div className='p-3 rounded-lg shadow-md mt-2 mb-6 font-light  '>
-                            <p className='text-sm text-gray-600 mb-1 text-right '> 🤖 🤝...</p>
+                    {msg.score >= 2 && msg.text.trim() !== '' && (
+                        <div className='p-3 rounded-lg shadow-md mt-2 mb-6 font-light  ml-36 fade-in'>
+                            <p className='text-sm text-gray-600 mb-1 text-right '> ...🤖 🤝</p>
                             <p className=" text-sm font-light text-black mb-1">{"Love the positivity!"}</p>
                         </div>
                     )}
 
-                    {msg.score >= 0 && msg.score < 2 && (
-                        <div className='p-3 rounded-lg shadow-md mt-2 mb-6 font-light '>
-                            <p className='text-sm text-gray-600 mb-1 text-right'> 🤖...</p>
+                    {msg.score >= 0 && msg.score < 2 && msg.text.trim() !== '' && (
+                        <div className='opacity-0 p-3 rounded-lg shadow-md mt-2 mb-6 font-light ml-36 fade-in'>
+                            <p className='text-sm text-gray-600 mb-1 text-right'> ...🤖</p>
                             <p className=" text-sm font-light text-black mb-1">{"If you’d like, you could add a bit more detail or emotion to make it more engaging and relatable"}</p>
                         </div>
                     )}
 
-                    {msg.score < 0 && (
+                    {msg.score < 0 && msg.text.trim() !== '' && (
 
-                        <div className='p-3 rounded-lg shadow-md mt-2 mb-6 font-light '>
-                            <p className='text-sm text-gray-600 mb-1  text-right'>🤖...</p>
+                        <div className='p-3 rounded-lg shadow-md mt-2 mb-6 font-light ml-36 fade-in'>
+                            <p className='text-sm text-gray-600 mb-1  text-right'>...🤖</p>
                             <p className="text-sm text-black">{" I hear you—that sounds frustrating. Maybe shifting the focus to what could help or improve the situation might make it feel a little lighter."}</p>
                         </div>
 
                     )}
 
-                    <p className="text-md text-gray-600 mb-1 font-semibold">{` 👤... ${msg.text}`}</p>
-                    <div className="text-xs text-black mt-1 italic">
+                 
+                    <p className="text-md text-gray-600 mb-2 font-semibold">{` 👤 ...`}</p>
+                    <p className="text-md text-gray-600 mb-2 font-semibold">{ msg.text}</p>
+                    <div className="text-xs text-black mt-2 italic border-t-2 border-dotted  border-gray-400 pt-2">
                         <span>Sentiment: {msg.sentiment}</span>
                         <span className="ml-1">Score: {msg.score}</span>
                         {msg.language && (
